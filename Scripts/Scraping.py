07aac2a90c0ab84b12c57515315373a522327521
Navneet Singh Arora <navneetsingh790@gmail.com>
2020-06-27 16:30:57 +0530

    Code Refactoring Part 2
    Code Refactoring Part 2

--- Scripts/Scraping.py
+++ Scripts/Scraping.py
@@ -1,6 +1,5 @@
 import logging
 from datetime import datetime
-from SetupEnvironment import SetupEnvironment
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait, Select
@@ -9,6 +8,7 @@ from selenium.common.exceptions import TimeoutException
 import time
 import requests
 from bs4 import BeautifulSoup
+import re
 
 """ Setting Basic Logging Options """
 LOG_FILENAME = datetime.now().strftime('../Logs/scraping_app_%d_%m_%Y.log')
@@ -20,39 +20,42 @@ logger.setLevel(logging.DEBUG)
 """ Download the ChromDriver in the /usr/local/bin """
 """ Run the following command to give all the rights to the executable : xattr -d com.apple.quarantine chromedriver """
 
-""" Going to the Main Website URL  """
-
 url = "https://www.kijiji.ca"
-browser = webdriver.Chrome()
-browser.get(url)
+driver = webdriver.Chrome()
+driver.get(url)
 
-""" Selecting a Particular Location And Area """
+driver.find_element(By.LINK_TEXT, "Alberta").click()
+driver.find_element(By.LINK_TEXT, "Calgary").click()
 
-browser.find_element(By.LINK_TEXT,"Alberta").click()
-browser.find_element(By.LINK_TEXT,"Calgary").click()
+driver.find_element_by_id("LocUpdate").click()
 
-browser.find_element_by_id("LocUpdate").click()
-
+""" WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, "SearchSubmit"))) """
 time.sleep(2)
+driver.find_element_by_id("SearchKeyword").send_keys("Office Space")
+driver.find_element_by_name("SearchSubmit").click()
 
-""" Searching a Particular Keyword """
+url_scrap = driver.current_url
 
-browser.find_element_by_id("SearchKeyword").send_keys("Office Space")
-browser.find_element_by_name("SearchSubmit").click()
-
-url_scrap = browser.current_url
 response = requests.get(url_scrap)
-print(response)
 
-""" soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, 'html.parser')
 
-post = soup.find_all(class_='title')
+post = soup.find_all('a', {'href': re.compile(
+    r'["/-a-zA-Z0-9]*')}, {'class_': re.compile(r'title')})
 
-title_list=[]
-for item in post:
-    title = item.get_text()
-    title_list.append(title.strip())
+# post = soup.find_all(re.compile("^a.href"))
 
-print(title_list) """
+fi = open('file.txt', 'w')
 
-browser.quit()
+for tag in post:
+    fi.write(str(tag.get_text()))
+
+fi.close()
+
+
+# title_list = []
+# for item in post:
+#     title = item.get_text()
+#     title_list.append(title.strip())
+
+# print(title_list)
